e7cd6502e3444faa8039ad3f0abe0581780818ba
shaosh <shaosh0913@gmail.com>
2014-04-13 23:59:00 -0700

    final commit

---
+++ SecureBankSystem/src/com/asubank/model/transfer/TransactionErrorCode.java
@@ -0,0 +1,6 @@
+package com.asubank.model.transfer;
+
+public class TransactionErrorCode {
+	public static final String OVERDRAFT = "Overdraft is not allowed";
+	public static final String NOTYOURACCOUNT = "You don't have this account";
+}
